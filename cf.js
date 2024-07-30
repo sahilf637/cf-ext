5f3339fa9520c7fcf10d52b27fc22f49d6e75912
sahilf637 <sahilfartyal3@gmail.com>
2024-07-30 18:20:45 +0530

    done

--- cf.js
+++ cf.js
@@ -1,19 +1,51 @@
-const header = document.querySelector(".problem-statement .header .title");
-var time = "00:00:00";
-setInterval(function () {
-  hour = Number(time.substr(0, 2));
-  min = Number(time.substr(3, 2));
-  sec = Number(time.substr(5, 2));
+const header = document.querySelector(".problem-statement .header");
+const URL = window.location.href;
+const timer = document.createElement("div");
+var time = localStorage.getItem(URL);
+if (!time) time = "00:00:00";
+timer.textContent = time;
+timer.className = "timerC";
+header.appendChild(timer);
+//console.log(header);
+const add = (a) => {
+  if (a[1] != "9") {
+    a = a[0] + (parseInt(a[1]) + 1).toString();
+  } else {
+    a = (parseInt(a[0]) + 1).toString() + "0";
+  }
+  return a;
+};
+const Interval = () => {
+  timeInterval = setInterval(function () {
+    hour = time.substr(0, 2);
+    min = time.substr(3, 2);
+    sec = time.substr(6, 2);
 
-  sec = sec + 1;
-  if (sec == "60") {
-    min = min + 1;
-    sec = "00";
-  }
-  if (min == "60") {
-    hour = hour + 1;
-    min = "00";
-  }
-  time = hour + ":" + min + ":" + sec;
-  console.log(hour, min, sec);
-}, 1000);
+    //console.log(hour, min, sec);
+    sec = add(sec);
+    if (sec == "60") {
+      min = add(min);
+      sec = "00";
+    }
+    if (min == "60") {
+      hour = add(hour);
+      min = "00";
+    }
+    time = hour + ":" + min + ":" + sec;
+    document.querySelector(".problem-statement .header .timerC").textContent =
+      time;
+  }, 1000);
+};
+var timeInterval;
+Interval();
+window.onfocus = function () {
+  Interval();
+};
+
+window.onblur = function () {
+  clearInterval(timeInterval);
+};
+
+window.addEventListener("beforeunload", function (e) {
+  localStorage.setItem(URL, time);
+});
